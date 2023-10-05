e19d5a0e4d4b56352384028f93d54c8f8f86bd77
Katka6 <147032354+Katka6@users.noreply.github.com>
2023-10-05 09:05:38 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("What is your name?");
+string wow = Console.ReadLine();
+if (wow == "pavel")
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+Console.WriteLine("Pick a number");
+double number1 = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Pick a number");
+double number2 = Convert.ToDouble(Console.ReadLine());
+double bla = number1 + number2;
+Console.WriteLine("blabla "+ bla );
+
+
+
+
+
+
+
+
+
+
