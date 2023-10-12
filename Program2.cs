cda15066ce9ecea3b3d796033824bfe08726dbb2
Katka6 <147032354+Katka6@users.noreply.github.com>
2023-10-12 09:40:03 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("fizzbuzz") ;
+
+
+
+
+
+for (int d = 0; d <= 20; d++)
+{ if (d % 15 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+            }
+    else{  
+        if (d % 3 == 0)
+        {
+            Console.WriteLine("fizz");
+        }
+        else
+        {
+            if (d % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            else
+            {
+                Console.WriteLine(d);
+            };
+        };
+    };
+};
+
+
+Console.WriteLine("Pick a number");
+
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Pick a number");
+
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Pick a number");
+
+string number 3 = Console.ReadLine ();
+
+if (number3 =  )
+{
+    double bla = number1 + number2;
+    Console.WriteLine("v " + bla);
+}
+    else (number3 = );
+{
+    double wow = number1 - number2;
+    Console.WriteLine("v " + wow);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
