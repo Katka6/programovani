c84504df57348c8fc4e9f85fa68961b2c9b593b9
Katka6 <147032354+Katka6@users.noreply.github.com>
2023-10-19 08:00:48 +0200

    Add files via upload

---
+++ kalkulacka.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Pick a number");
+
+double N1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Pick a number");
+
+double N2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Pick a op");
+
+string N3 = Console.ReadLine ();
+
+if (N3 == "+")
+{
+    double u = N1 + N2;
+    Console.WriteLine("v " + u);
+}
+
+    if (N3 == "-") ;
+    {
+        double t = N1 - N2;
+        Console.WriteLine("v " + t);
+    }
+ 
+    if (N3 == "*")
+    {
+        double q = N1 * N2;
+        Console.WriteLine("v " + q);
+    }
+
+
+
+    if (N3 == "/")
+    {
+        double w = N1 / N2;
+        Console.WriteLine("v " + w);
+    }
+
+    if (N3 == "^")
+    {
+        double e = 1;
+        for (int i = 0; i < N2; i++)
+            e = N1 * e;
+        Console.WriteLine("v " + e);
+    }
+
+
+    if (N3 == "%")
+    {
+        double e = N1 % N2;
+        Console.WriteLine("v " + e);
+    }
+
+
+
