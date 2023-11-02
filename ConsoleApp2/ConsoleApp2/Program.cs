648eb4c0214688215c3032b9333e4fe1af4a6d0e
Katka6 <147032354+Katka6@users.noreply.github.com>
2023-11-02 09:39:40 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
+
+
+
+Console.WriteLine("Pick a number");
+
+double N6 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Pick a number");
+
+double N7 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+double maximum(double x, double z)
+{
+    if (x < z)
+    {
+        return (z);
+    }
+
+    else if (x > z)
+    {
+        return (x);
+    }
+
+    else
+    {
+        return (x);
+     
+    }
+};
+
+Console.WriteLine(maximum (N6 , N7 ));
+
+
+
+
+
+
+int cislooduzivatele()
+{
+
+    Console.WriteLine("pick a number");
+    int input = Console.Read();
+
+    return input;
+}
+
+int cislo = cislooduzivatele();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
