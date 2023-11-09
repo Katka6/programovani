2616b8a2577ffa9202137156438b12d9f830634a
Katka6 <147032354+Katka6@users.noreply.github.com>
2023-11-09 09:43:23 +0100

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+int[] number = { 1, 2, 3 };
+int[] number2 = number;
+//int[] number2 = new int[] {1,2,3};
+//int[] number3 = new int[10];
+
+//úplně stelný u všeho ostatního
+Console.WriteLine(number);
+Console.WriteLine(number2);
+//Console.WriteLine(number[0]);
+
+void printarray (int[] array)
+{
+    for (int i = 0; i<3; i++) 
+    {
+        Console.Write(array[i]);
+    };
+};
+
+printarray (number);
+
+printarray(number2);
+
+
+
+void changefirst(ref int[] array, int newvalue)
+{
+    array[0] = newvalue;
+    array = new int[] { 20, 30, 20, 3 };
+}
+changefirst(ref number, 42);
+printarray (number);
+printarray (number2);
+
+int[] coefficients = new int[] { 6, 5, 4, -2, 4, 9 };
+
+
+void printpolynomial (int[] array)
+{
+    for (int d = 0; d<coefficients.Length; d++)
+    {
+        Console.Write(coefficients[d]);
+        Console.Write("+") ;
+        Console.Write(coefficients.Length - d - 1) ;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
