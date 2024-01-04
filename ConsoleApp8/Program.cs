8578cd906cbf39a7e05defe0ede63f7bd1cd18c1
Katka6 <147032354+Katka6@users.noreply.github.com>
2024-01-04 09:33:11 +0100

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+//Console.WriteLine("vyber soubor");
+//string path = Console.ReadLine();
+
+//try
+//{
+//  string text = File.ReadAllText(path);
+// Console.WriteLine(text);
+//}
+//catch (Exception ex)
+//{
+//  Console.WriteLine("ne, toto je tvá chyba:");
+//   Console.WriteLine(ex);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static string GetPathFromUser()
+{
+    Console.WriteLine("Enter path");
+    string path = Console.ReadLine();
+    return path;
+}
+
+static void PrintErrorMessage()
+{
+    Console.WriteLine("File does not exist");
+}
+
+static string GetFileContents(string path)
+{
+    return File.ReadAllText(path);
+}
+
+static void PrintFileContents(string path)
+{
+    try
+    {
+        string contents = GetFileContents(path);
+        Console.WriteLine(contents);
+    }
+    catch (Exception e)
+    {
+        PrintErrorMessage();
+    }
+}
+
+static void UserReadFile()
+{
+    string path = GetPathFromUser();
+
+    PrintFileContents(path);
+}
+
+
+UserReadFile();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
